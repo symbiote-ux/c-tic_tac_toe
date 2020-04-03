dd780ca08be935066555167dfc56c838606fe01d
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-04-03 12:16:06 +0530

    winning condition added

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -3,6 +3,21 @@
 
 void display_board(char *);
 void start_game(char *);
+int has_won(char, char *);
+
+int has_won(char name,char tiles[]) {
+  if(((tiles[0] == tiles[1]) && (tiles[1] == tiles[2]) && (tiles[1] == name)) ||
+     ((tiles[4] == tiles[5]) && (tiles[5] == tiles[6]) && (tiles[5] == name)) ||
+     ((tiles[7] == tiles[8]) && (tiles[8] == tiles[9]) && (tiles[8] == name)) ||
+     ((tiles[1] == tiles[4]) && (tiles[4] == tiles[7]) && (tiles[4] == name)) ||
+     ((tiles[2] == tiles[5]) && (tiles[5] == tiles[8]) && (tiles[5] == name)) ||
+     ((tiles[3] == tiles[6]) && (tiles[6] == tiles[9]) && (tiles[6] == name)) ||
+     ((tiles[1] == tiles[5]) && (tiles[5] == tiles[9]) && (tiles[5] == name)) ||
+     ((tiles[3] == tiles[5]) && (tiles[5] == tiles[7]) && (tiles[5] == name))) {
+       return 1;
+     }
+     return 0;
+};
 
 void start_game(char tiles[]) {
   int turn = 1,move,i=1;
@@ -12,6 +27,10 @@ void start_game(char tiles[]) {
     scanf("%d",&move);
     tiles[move -1] = 'H';
     display_board(tiles);
+    if(has_won('H',tiles)) {
+      printf("You Wins\n");
+      exit(0);
+    }
     turn =! turn;
   } 
   else {
@@ -19,6 +38,10 @@ void start_game(char tiles[]) {
   move = rand() % 9 + 1;
   tiles[move] = 'M';
   display_board(tiles);
+  if(has_won('M',tiles)) {
+    printf("Machine Won\n");
+    exit(0);
+  }
   turn =!turn;
   }
   }
