a600e11a4dfaf893cf14965ba572da86cbf18d1e
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-04-03 11:48:27 +0530

    start_game function added

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -5,20 +5,24 @@ void display_board(char *);
 void start_game(char *);
 
 void start_game(char tiles[]) {
-  int turn = 1,move;
+  int turn = 1,move,i=1;
+  while(i != 9) {
   if(turn) {
     printf("Human turn: Enter number between 1-9 \n");
     scanf("%d",&move);
     tiles[move -1] = 'H';
-    turn = !turn;
     display_board(tiles);
+    turn =! turn;
   } 
   else {
   printf("Machine Turn:\n");
   move = rand() % 9 + 1;
   tiles[move] = 'M';
   display_board(tiles);
+  turn =!turn;
   }
+  }
+  i++;
 }
 
 void display_board(char tiles[]) {
@@ -30,8 +34,9 @@ void display_board(char tiles[]) {
 }
 
 int main() {
+  int i;
   char tiles[9];
-  for(int i = 0; i < sizeof(tiles)/sizeof(char); i++){
+  for(i = 0; i < sizeof(tiles)/sizeof(char); i++){
     tiles[i] ='\0';
   }
   display_board(tiles);
